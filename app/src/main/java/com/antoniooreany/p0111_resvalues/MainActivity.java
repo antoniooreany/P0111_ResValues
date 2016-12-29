c533f56c58bdf39e90f914e8df5b3fbc343ea7bd
antoniooreany <antonio_oreany@mail.ru>
2016-12-29 15:29:59 +0300

    All variables of class MainActivity became private.

--- app/src/main/java/com/antoniooreany/p0111_resvalues/MainActivity.java
+++ app/src/main/java/com/antoniooreany/p0111_resvalues/MainActivity.java
@@ -5,15 +5,17 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    int imageViewIcon;
-    boolean imageViewIconBlack;
-    boolean imageViewIconLarge;
+    private static final int BLACK_48DP = 1;
+    private static final int GREY_48DP = 2;
+    private static final int BLACK_18DP = 3;
+    private static final int GREY_18DP = 4;
+    private ImageView imageView;
+    private int imageViewIcon;
+    private int imageViewIconType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +23,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.imageView);
         imageViewIcon = R.drawable.ic_android_black_48dp;
-        imageViewIconBlack = true;
-        imageViewIconLarge = true;
+        imageViewIconType = BLACK_48DP;
         imageView.setImageDrawable(getResources().getDrawable(imageViewIcon));
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (imageViewIconBlack && imageViewIconLarge) {
-                    imageViewIcon = R.drawable.ic_android_grey600_48dp;
-                    imageViewIconBlack = false;
-                    imageViewIconLarge = true;
-                } else if (!imageViewIconBlack && imageViewIconLarge){
-                    imageViewIcon = R.drawable.ic_android_black_18dp;
-                    imageViewIconBlack = true;
-                    imageViewIconLarge = false;
-                } else if (imageViewIconBlack && !imageViewIconLarge) {
-                    imageViewIcon = R.drawable.ic_android_grey600_18dp;
-                    imageViewIconBlack = false;
-                    imageViewIconLarge = false;
-                } else if (!imageViewIconBlack && !imageViewIconLarge) {
-                    imageViewIcon = R.drawable.ic_android_black_48dp;
-                    imageViewIconBlack = true;
-                    imageViewIconLarge = true;
+                switch (imageViewIconType) {
+                    case BLACK_48DP:
+                        imageViewIcon = R.drawable.ic_android_grey600_48dp;
+                        imageViewIconType = GREY_48DP;
+                        break;
+                    case GREY_48DP:
+                        imageViewIcon = R.drawable.ic_android_black_18dp;
+                        imageViewIconType = BLACK_18DP;
+                        break;
+                    case BLACK_18DP:
+                        imageViewIcon = R.drawable.ic_android_grey600_18dp;
+                        imageViewIconType = GREY_18DP;
+                        break;
+                    case GREY_18DP:
+                        imageViewIcon = R.drawable.ic_android_black_48dp;
+                        imageViewIconType = BLACK_48DP;
+                        break;
+
                 }
                 Resources resources = getResources();
                 Drawable drawable = resources.getDrawable(MainActivity.this.imageViewIcon);
